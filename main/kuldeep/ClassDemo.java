73ed3afa3fdf73879a4deb5c39db00ecf4cc2b1e
kuldeepsharma1 <hytek112@gmail.com>
2022-07-06 09:27:21 +0000

    collage

---
+++ main/kuldeep/ClassDemo.java
@@ -0,0 +1,14 @@
+public class ClassDemo {
+    int x = 5;
+    public static void main(String[] args) {
+       ClassDemo Obj1 = new ClassDemo();
+       ClassDemo Obj2 = new ClassDemo();
+       ClassDemo Obj3 = new ClassDemo();
+       System.out.println("I am Object 1"+Obj1.x);
+       System.out.println("I am Object 2"+Obj2.x);
+       System.out.println("I am Object 3"+Obj3.x);
+       
+          
+          
+    }
+}
